17a0368546a069824d3bae6952700add60b81a30
Johnny Gabritchidze <jgabritchidze@evex.ge>
2024-01-24 16:20:29 +0400

    commit

---
+++ restaurant/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Booking, Menu
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = ['id', 'title', 'price', 'inventory']
+        
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields =  '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Booking, Menu
+from serializers import MenuSerializer, BookingSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'Get':
+            permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in permission_classes]
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method != 'Get':
+            permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in permission_classes]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
